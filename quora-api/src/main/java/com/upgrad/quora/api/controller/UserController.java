3ca4d39fdadd2289932ea35e21f28e5b1382b778
Abhishek Dutta <dutta.abhishek9394@gmail.com>
2020-05-30 01:21:57 +0530

    API Implementation - signin - "/user/signin"
    This endpoint is used for user authentication. The user authenticates in the application and after successful authentication, JWT token is given to a user.

--- quora-api/src/main/java/com/upgrad/quora/api/controller/UserController.java
+++ quora-api/src/main/java/com/upgrad/quora/api/controller/UserController.java
@@ -1,18 +1,25 @@
 package com.upgrad.quora.api.controller;
 
 
+import com.upgrad.quora.api.model.SigninResponse;
 import com.upgrad.quora.api.model.SignupUserRequest;
 import com.upgrad.quora.api.model.SignupUserResponse;
 import com.upgrad.quora.service.business.UserAuthenticationService;
+import com.upgrad.quora.service.entity.UserAuthEntity;
 import com.upgrad.quora.service.entity.UserEntity;
+import com.upgrad.quora.service.exception.AuthenticationFailedException;
 import com.upgrad.quora.service.exception.SignUpRestrictedException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Base64;
 
 @RestController
 @RequestMapping("/")
@@ -50,5 +57,33 @@ public class UserController {
         UserEntity createdUserEntity = userAuthService.signup(userEntity);
         SignupUserResponse userResponse = new SignupUserResponse().id(createdUserEntity.getUuid()).status("USER SUCCESSFULLY REGISTERED");
         return new ResponseEntity<SignupUserResponse>(userResponse, HttpStatus.CREATED);
+    }
+
+    /**
+     * This method is for a user to singin.
+     *
+     * @param authorization is basic auth (base 64 encoded). Usage: Basic <Base 64 Encoded username:password>
+     * @return SigninResponse which contains user id and a access-token in the response header.
+     * @throws AuthenticationFailedException ATH-001 if username doesn't exist, ATH-002 if password is wrong.
+     */
+    @RequestMapping(
+            method = RequestMethod.POST,
+            path = "/user/signin",
+            produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    public ResponseEntity<SigninResponse> signin(@RequestHeader("authorization") final String authorization)throws AuthenticationFailedException {
+
+        byte[] decode = Base64.getDecoder().decode(authorization.split("Basic ")[1]);
+        String decodedText = new String(decode);
+        String[] decodedArray = decodedText.split(":");
+        UserAuthEntity userAuthEntity = userAuthService.signin(decodedArray[0], decodedArray[1]);
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("access-token", userAuthEntity.getAccessToken());
+
+        SigninResponse signinResponse = new SigninResponse();
+        signinResponse.setId(userAuthEntity.getUserEntity().getUuid());
+        signinResponse.setMessage("SIGNED IN SUCCESSFULLY");
+
+        return new ResponseEntity<SigninResponse>(signinResponse, headers, HttpStatus.OK);
     }
 }
--- quora-service/src/main/java/com/upgrad/quora/service/business/UserAuthenticationService.java
+++ quora-service/src/main/java/com/upgrad/quora/service/business/UserAuthenticationService.java
@@ -1,13 +1,17 @@
 package com.upgrad.quora.service.business;
 
+import com.upgrad.quora.service.dao.UserAuthDao;
 import com.upgrad.quora.service.dao.UserDao;
+import com.upgrad.quora.service.entity.UserAuthEntity;
 import com.upgrad.quora.service.entity.UserEntity;
+import com.upgrad.quora.service.exception.AuthenticationFailedException;
 import com.upgrad.quora.service.exception.SignUpRestrictedException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.time.ZonedDateTime;
 import java.util.UUID;
 
 @Service
@@ -15,6 +19,9 @@ public class UserAuthenticationService {
 
     @Autowired
     private UserDao userDao;
+
+    @Autowired
+    private UserAuthDao userAuthDao;
 
     @Autowired
     private PasswordCryptographyProvider passwordCryptographyProvider;
@@ -41,6 +48,45 @@ public class UserAuthenticationService {
         userEntity.setSalt(encryptedText[0]);
         userEntity.setPassword(encryptedText[1]);
         return userDao.createUser(userEntity);
+    }
+
+    /**
+     * This method is used for the user to signin.
+     *
+     * @param username Username of the user who is singing in.
+     * @param password Credentials of the user who is singing in
+     * @return UserAuthEntity which contains the access-token and other details.
+     * @throws AuthenticationFailedException ATH-001 if the username doesn't exist in DB or ATH-002 if
+     *     the password is wrong.
+     */
+    @Transactional(propagation = Propagation.REQUIRED)
+    public UserAuthEntity signin(final String username, final String password)
+            throws AuthenticationFailedException {
+
+        UserEntity userEntity = userDao.getUserByUserName(username);
+        if (userEntity == null) {
+            throw new AuthenticationFailedException("ATH-001", "This username does not exist");
+        }
+        final String encryptedPassword =passwordCryptographyProvider.encrypt(password, userEntity.getSalt());
+        if (!encryptedPassword.equals(userEntity.getPassword())) {
+            throw new AuthenticationFailedException("ATH-002", "Password failed");
+        }
+
+        JwtTokenProvider jwtTokenProvider = new JwtTokenProvider(encryptedPassword);
+        UserAuthEntity userAuthEntity = new UserAuthEntity();
+        userAuthEntity.setUuid(UUID.randomUUID().toString());
+        userAuthEntity.setUserEntity(userEntity);
+        final ZonedDateTime now = ZonedDateTime.now();
+        final ZonedDateTime expiresAt = now.plusHours(8);
+        userAuthEntity.setAccessToken(
+                jwtTokenProvider.generateToken(userEntity.getUuid(), now, expiresAt));
+        userAuthEntity.setLoginAt(now);
+        userAuthEntity.setExpiresAt(expiresAt);
+
+        userAuthDao.createAuthToken(userAuthEntity);
+        userDao.updateUserEntity(userEntity);
+
+        return userAuthEntity;
     }
 
     // checks whether the username exist in the database
